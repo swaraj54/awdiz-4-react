9300b1133fb9f414debb1efcff7937a6522bb6a9
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-09-27 20:53:10 +0530

    uPdates

--- frontend/src/components/22-09/Register2.jsx
+++ frontend/src/components/22-09/Register2.jsx
@@ -1,16 +1,36 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Register2 = () => {
   const [userData, setUserData] = useState({ name: "", email: "", password: "" });
-
+  const router = useNavigate();
   // console.log(userData,"userdata")
 
   const handleChange = (event) => {
-    console.log(event.target.value, "value", event.target.name, "name")
+    // console.log(event.target.value, "value", event.target.name, "name")
+    setUserData({ ...userData, [event.target.name]: event.target.value })
   }
 
-  const sendDataToBackend = () => {
-    alert("Data submitted to backend..")
+  const sendDataToBackend = async (event) => {
+    event.preventDefault();
+    // alert("Data submitted to backend..")
+    if (userData.name && userData.email && userData.password) {
+      if (userData.password.length >= 8) {
+        // const response = await axios.post("http://localhost:8000/register", { userData });
+        const response = { data: { success: true } };
+        if (response.data.success) {
+          alert("Registeration successfull.")
+          setUserData({ name: "", email: "", password: "" })
+          router("/")
+        } else {
+          alert(response.data.error)
+        }
+      } else {
+        alert("Password must be 8 digit.")
+      }
+    } else {
+      alert("Please fill the all values..")
+    }
   }
 
   return (
@@ -23,7 +43,7 @@ const Register2 = () => {
         <input name='email' type='email' onChange={handleChange} /> <br />
         <label>Password :</label><br />
         <input name='password' type='password' onChange={handleChange} /> <br />
-        <input type='submit' /> <br />
+        <input type='submit' value="Register here" /> <br />
       </form>
     </div>
   )
