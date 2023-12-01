0c2490a982317a423af334b1e97cdd780befb955
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-12-01 21:08:24 +0530

    Updates

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -2,15 +2,37 @@ import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from './Context/AuthContext';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import api from '../helpers/AxiosConfig';
 
 const Cart = () => {
-    const { cartProducts, setCartProducts } = useState([]);
+    const [cartProducts, setCartProducts] = useState([]);
+    console.log(cartProducts, "cartProducts")
 
     const { state } = useContext(AuthContext);
     const router = useNavigate();
 
     async function getYourCartProducts() {
-        alert("Inside function") // assignment - complete this bloack
+        // alert("Inside function") // assignment - complete this bloack
+        try {
+            const response = await api.post('/user/cart', { id: state?.user?.id })
+            if (response.data.success) {
+                setCartProducts(response.data.products)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function deleteProduct(productId) {
+        try {
+            const response = await api.post('/user/delete-cart', { productId, userId: state?.user?.id })
+            if (response.data.success) {
+                toast.success(response.data.message)
+                setCartProducts(response.data.products)
+            }
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     useEffect(() => {
@@ -23,13 +45,24 @@ const Cart = () => {
             if (state?.user?.id) {
                 getYourCartProducts()
             }
-
         }
     }, [state])
 
 
     return (
-        <div>Cart</div>
+        <div>
+            <h1>Cart</h1>
+            {cartProducts.length ? <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                {cartProducts.map((product) => (
+                    <div>
+                        <img src={product.image} />
+                        <h1>{product.name}</h1>
+                        <h1>${product.price}/-</h1>
+                        <button onClick={() => deleteProduct(product._id)}>Delete</button>
+                    </div>
+                ))}
+            </div> : <div>Loading..</div>}
+        </div>
     )
 }
 
