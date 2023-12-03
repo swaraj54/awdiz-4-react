0b540a5122d71c63d3b80bb5b8611073d53e8add
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-12-03 16:23:21 +0530

    Updatees

--- frontend/src/helpers/AxiosConfig.js
+++ frontend/src/helpers/AxiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 var BackendUrl = "http://localhost:8000/api/v1"
 if (process.env.MODE == "PRODUCTION") {
-    BackendUrl = "https://awdiz-4.com/api/v1"
+    BackendUrl = "https://awdiz-backend-4.onrender.com/api/v1"
 }
 
 const api = axios.create({ baseURL: BackendUrl })
